0fabad2c827701fd9b7ec35c81b413a31a5e04ad
madbull12 <huangandrian02@gmail.com>
2022-12-14 00:09:16 +0700

    trend link

--- src/components/TrendComponent.tsx
+++ src/components/TrendComponent.tsx
@@ -1,19 +1,24 @@
-import { Hashtag, Prisma } from '@prisma/client'
-import React from 'react'
+import { Hashtag, Prisma } from "@prisma/client";
+import Link from "next/link";
+import React from "react";
 
 type HashtagWithTweet = Prisma.HashtagGetPayload<{
-    include:{
-        tweets:true
-    }
-}>
-const TrendComponent = ({ hashtag }: { hashtag:HashtagWithTweet }) => {
+  include: {
+    tweets: true;
+  };
+}>;
+const TrendComponent = ({ hashtag }: { hashtag: HashtagWithTweet }) => {
   return (
-    <div className='p-2 space-y-1 hover:bg-gray-50 cursor-pointer'>
-        <p className='text-xs md:text-base text-gray-500'>Trending</p>
-        <p className='font-semibold text-lg'>{hashtag.name}</p>
-        <p className='text-gray-500 text-sm md:text-base'>{hashtag.tweets.length} Tweets</p>
-    </div>
-  )
-}
+    <Link href={`/search?q=${hashtag.name}&f=top`}>
+      <div className="cursor-pointer space-y-1 p-2 hover:bg-gray-50">
+        <p className="text-xs text-gray-500 md:text-base">Trending</p>
+        <p className="text-lg font-semibold">{hashtag.name}</p>
+        <p className="text-sm text-gray-500 md:text-base">
+          {hashtag.tweets.length} Tweets
+        </p>
+      </div>
+    </Link>
+  );
+};
 
-export default TrendComponent
+export default TrendComponent;
