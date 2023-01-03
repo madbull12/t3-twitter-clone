2e0a78782c7ab68b080f2e985829bc743a53c8fa
madbull12 <huangandrian02@gmail.com>
2023-01-03 16:25:44 +0700

    dropdown  tweet

--- src/components/MenuDropdown.tsx
+++ src/components/MenuDropdown.tsx
@@ -1,23 +1,36 @@
+import { useSession } from "next-auth/react";
 import React from "react";
 import { BiDotsHorizontal } from "react-icons/bi";
+import { trpc } from "../utils/trpc";
 
-const MenuDropdown = () => {
+const MenuDropdown = ({ tweetId }: { tweetId: string }) => {
+  const { data: session } = trpc.auth.getSession.useQuery();
+
+  const { data: userTweets } = trpc.tweet.getUserTweets.useQuery({
+    userId: session?.user?.id as string,
+    link: "",
+  });
+
+  const isYourTweet = !!userTweets?.find((tweet) => tweet.id === tweetId);
+
   return (
     <div className="dropdown">
       <label tabIndex={0} className=" cursor-pointer ">
         <BiDotsHorizontal className="text-xl text-gray-400" />
       </label>
-      <ul
-        tabIndex={0}
-        className="dropdown-content menu rounded-box w-52 bg-base-100 p-2 shadow"
-      >
-        <li >
-          <a className="text-red-500">Delete</a>
-        </li>
-        {/* <li>
-          <a>Item 2</a>
-        </li> */}
-      </ul>
+      {isYourTweet ? (
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu rounded-box w-52 bg-base-100 p-2 shadow"
+        >
+          <li>
+            <a className="text-red-500">Delete</a>
+          </li>
+          {/* <li>
+       <a>Item 2</a>
+     </li> */}
+        </ul>
+      ) : null}
     </div>
   );
 };
--- src/components/TweetComponent.tsx
+++ src/components/TweetComponent.tsx
@@ -88,6 +88,8 @@ const TweetComponent = ({ tweet }: IProps) => {
     }
   };
 
+  
+
   return (
     <div
       onClick={() => {
@@ -100,7 +102,7 @@ const TweetComponent = ({ tweet }: IProps) => {
       className="flex relative cursor-pointer items-start gap-x-2 p-2 transition-all duration-100 ease-in-out hover:bg-gray-100 md:gap-x-4 md:p-4"
     >
       <div className="absolute top-2 right-2" onClick={(e)=>e.stopPropagation()}>
-        <MenuDropdown />
+        <MenuDropdown tweetId={tweet.id} />
       </div>
       <div className="flex-[0.1] ">
         <Avatar image={tweet.user.image || ""} width={40} height={40} />
