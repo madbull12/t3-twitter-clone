f31ec4d67e5e705ae8a4dbec5ab16986c68fcb27
madbull12 <huangandrian02@gmail.com>
2023-01-08 01:15:47 +0700

    ui

--- src/components/NavFeed.tsx
+++ src/components/NavFeed.tsx
@@ -1,12 +1,29 @@
 import { Tweet } from "@prisma/client";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React from "react";
 import { BsArrowLeft } from "react-icons/bs";
+import useMediaQuery from "../../hooks/useMediaQuery";
+import Avatar from "./Avatar";
 
 const NavFeed = ({ title, tweets }: { title: string; tweets?: number }) => {
   const router = useRouter();
+  const { data: session } = useSession();
+  const phone = useMediaQuery("(min-width:768px)");
   return (
     <nav className="sticky top-0 z-50  flex w-full items-center gap-x-4 bg-base-100/30 p-2  backdrop-blur-lg  md:gap-x-4">
+      {!phone ? (
+        <>
+          {router.pathname === "/" ? (
+            <Avatar
+              image={session?.user?.image as string}
+              width={30}
+              height={30}
+            />
+          ) : null}
+        </>
+      ) : null}
+
       {router.pathname !== "/" ? (
         <BsArrowLeft
           className="cursor-pointer text-xl"
