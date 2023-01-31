8e4f5df6253734a58d5025a2685e4ea1e7f8a1d4
madbull12 <huangandrian02@gmail.com>
2023-01-31 13:58:58 +0700

    mobile nav active

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,24 +1,32 @@
 import Link from "next/link";
 import React from "react";
-import { AiFillHome, AiFillBell, AiOutlineMail } from "react-icons/ai";
+import { AiFillHome, AiFillBell, AiOutlineMail, AiOutlineHome, AiOutlineBell } from "react-icons/ai";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Button from "./Button";
 import Profile from "./Profile";
 import useMediaQuery from "../../hooks/useMediaQuery";
 import { BiBookmark, BiSearch, BiUser } from "react-icons/bi";
 import { BsMailbox } from "react-icons/bs";
+import { useRouter } from "next/router";
+import { FaSearch } from "react-icons/fa";
 const MobileNav = () => {
+
+  const router = useRouter();
+
 
   return (
     <div className='fixed border-t text-3xl border-base-300 z-[1000] flex items-center justify-between bottom-0 right-0 w-full left-0 px-8 py-4 bg-base-100'>
-        <Link href="/">
-            <AiFillHome />
+        <Link href={"/"}>
+            {router.pathname==="/" ?  <AiFillHome /> : <AiOutlineHome />}
+           
         </Link>
         <Link href="/explore">
-            <BiSearch />
+            {router.pathname==="/explore" ? <FaSearch /> : <BiSearch />}
+            
         </Link>
-        <Link href="/">
-            <AiFillBell />
+        <Link href="/notifications">
+            {router.pathname==="/notifications" ?<AiFillBell /> : <AiOutlineBell /> }
+            
         </Link>
         <Link href="/">
             <AiOutlineMail />
